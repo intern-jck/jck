bc402f869f725498cea520e3dee2e8e28d76898b
Justin Kirk <justin.c.kirk@gmail.com>
2026-01-19 23:16:15 -0500

    refactors vortex js

--- js/vortex-graph.js
+++ js/vortex-graph.js
@@ -1,65 +1,446 @@
-const colors = ["red", "green", "blue", "magenta", "cyan"];
-function mouseOver(x, y, w, h) {
-    if (x < 0 || y < 0 || x > w || y > h) {
-        return false;
-    }
+const colors = ["red", "green", "blue", "magenta", "cyan", "yellow", "orange", "purple", "navy", "hotpink", "'chartreuse'"];
 
-    if (x < w && y < h) {
-        return true;
-    }
+
+let width = 0;
+let height = 0;
+let centerX = 0;
+let centerY = 0;
+
+
+let pi = Math.PI;
+let tau = 2 * pi;
+let modulus = 16;
+let multiplier = 2;
+
+let diameter = 0;
+let radius = 0;
+let center = 0;
+
+// let vortex = null;
+// function vortexGraphSketch(p) {
+
+function setup() {
+
+    let c = document.getElementById("vortex");
+    width = c.clientWidth;
+    height = c.clientHeight;
+    centerX = width / 2;
+    centerY = height / 2;
+    radius = width / 2 - 10;
+    diameter = radius * 2;
+    center = width / 2;
+
+    colorMode(HSL)
+    const canvas = createCanvas(width, height, SVG);
+    canvas.parent('vortex');
+
+    strokeWeight(2)
+
+    createVortex(center, radius, multiplier, modulus);
+    // circle(this.center, this.center, this.diameter)
+
+    // vortex = new Vortex(p, width, height);
+    // vortex.init();
+    // p.frameRate(30);
+
+    // let modulusSlider = p.createSlider(2, 100);
+    // modulusSlider.addClass("vortex-slider");
+    // modulusSlider.input(modulusSliderUpdate);
+    // modulusSlider.size(400);
+
+    // let multiplierSlider = p.createSlider(2, 10);
+    // multiplierSlider.addClass("vortex-slider");
+    // multiplierSlider.input(multiplierSliderUpdate);
+    // multiplierSlider.size(400);
 }
 
-/**
- * Vortex Graph Sketch
- */
-function vortexGraphSketch(p) {
-    let width = 0;
-    let height = 0;
-    let vortex = null;
-
-    p.setup = () => {
-
-        let c = document.getElementById("vortex");
-        width = c.clientWidth;
-        height = c.clientHeight;
-
-        const canvas = p.createCanvas(width, height);
-        canvas.parent('vortex');
-        p.background(255);
-
-        vortex = new Vortex(p, width, height);
-        vortex.init();
-        p.frameRate(30);
-
-        // let modulusSlider = p.createSlider(2, 100);
-        // modulusSlider.addClass("vortex-slider");
-        // modulusSlider.input(modulusSliderUpdate);
-        // modulusSlider.size(400);
-
-        // let multiplierSlider = p.createSlider(2, 10);
-        // multiplierSlider.addClass("vortex-slider");
-        // multiplierSlider.input(multiplierSliderUpdate);
-        // multiplierSlider.size(400);
-    }
-
-    p.draw = () => {
-    }
-
-    // modulusSliderUpdate = () => {
-    //     let val = modulusSlider.value();
-    //     console.log("vortex", val);
-    //     vortex.modulus = val;
-
-    //     vortex.createVortex()
-    // }
-
-    // multiplierSliderUpdate = () => {
-    //     let val1 = multiplierSlider.value();
-    //     console.log("vortex", val1);
-    //     vortex.multiplier = val1;
-
-    //     vortex.createVortex()
-    // }
+function draw() {
 }
 
-new p5(vortexGraphSketch, document.getElementById("vortex"));
+
+function clearDrawing() {
+    let canvas = document.getElementById(vortex);
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function createVortex(c, r, m, n) {
+    // let lines = [];
+    // let colors = this.createColors();
+
+    // let canvas = document.getElementById('vortex');
+    // let ctx = canvas.getContext('2d');
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    clear()
+    stroke(0)
+    ellipse(centerX, centerY, diameter, diameter);
+    for (let i = 0; i < n; i++) {
+        // Alpha is remainder after division.
+        // It is also the digital root for base m
+        let alpha = (i * m) % n;
+        if (alpha != i) {
+
+            // Get x,y cords for two points along circle.
+            // Starting point.
+            let x1 = c + r * Math.sin((i * 2 * Math.PI) / n);
+            let y1 = c - r * Math.cos((i * 2 * Math.PI) / n);
+
+            // Ending point.
+            let x2 = c + r * Math.sin((m * i * 2 * Math.PI) / n);
+            let y2 = c - r * Math.cos((m * i * 2 * Math.PI) / n);
+
+            // Create lines between these two points.
+            // let line = this.svgLine(x1, y1, x2, y2);
+            // console.log("line", x1, y1, x2, y2)
+
+            let lineLength = Math.floor(dist(x1, y1, x2, y2))
+            // console.log(lineLength, remap(lineLength, 0, width, 0, 10))
+            let lineColor = Math.floor(remap(lineLength, 0, width, 0, 360))
+            console.log(lineLength, lineColor)
+
+            // stroke(colors[lineColor]);
+            stroke(lineColor, 100, 50)
+            strokeWeight(1);
+            line(x1, y1, x2, y2);
+
+
+
+            // Set color and add to svg...
+            // let lineLength = Math.floor(
+            //     Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2).toFixed(2)
+            // );
+
+            // let colorNumber = this.remapInt(
+            //     lineLength,
+            //     0,
+            //     this.diameter,
+            //     96,
+            //     0
+            // );
+            // console.log(colors[colorNumber]);
+            // console.log(colorNumber)
+            // let max = 0xEEEEEE;
+            // let result = Math.round(max / colorNumber).toString(16);
+            // const linecolor = `hsl(${lineLength * 2}, 100%, 50%)`; // Color based on length
+
+            // line.setAttribute('stroke', colors[colorNumber]);
+            // line.setAttribute("stroke", linecolor);
+            // lines.push(line);
+        }
+    }
+    // return lines;
+}
+
+function updateModulus(value) {
+    console.log(value)
+    modulus = value;
+    createVortex(center, radius, multiplier, modulus);
+}
+
+function updateMultiplier(value) {
+    console.log(value)
+    multiplier = value;
+    createVortex(center, radius, multiplier, modulus);
+}
+
+function remap(val, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (val - low1) / (high1 - low1);
+}
+
+// modulusSliderUpdate = () => {
+//     let val = modulusSlider.value();
+//     console.log("vortex", val);
+//     vortex.modulus = val;
+
+//     vortex.createVortex()
+// }
+
+// multiplierSliderUpdate = () => {
+//     let val1 = multiplierSlider.value();
+//     console.log("vortex", val1);
+//     vortex.multiplier = val1;
+
+//     vortex.createVortex()
+// }
+// }
+
+// new p5(vortexGraphSketch, document.getElementById("vortex"));
+
+
+class Vortex {
+    constructor(p, w, h) {
+        this.p5 = p;
+        this.pi = Math.PI;
+        this.tau = 2 * this.pi;
+        this.modulus = 16;
+        this.multiplier = 2;
+
+        // this.svgWidth = w;
+        // this.svgHeight = h;
+
+        this.width = w;
+        this.height = h;
+
+        this.diameter = this.width - 10;
+        this.radius = this.diameter / 2;
+        this.diameter = this.radius * 2;
+        this.center = this.width / 2;
+    }
+
+    init() {
+        this.createVortex();
+    }
+
+    createVortex() {
+        // console.log(this.multiplier, this.modulus)
+        this.p5.circle(this.center, this.center, this.diameter)
+        this.createLines(this.center, this.radius, this.multiplier, this.modulus);
+    }
+
+    // Creates a array of svg lines based on inputs:
+    // center, radius, mulitplier and modulus.
+    createLines(c, r, m, n) {
+        // let lines = [];
+        // let colors = this.createColors();
+
+        for (let i = 0; i < n; i++) {
+            // Alpha is remainder after division.
+            // It is also the digital root for base m
+            let alpha = (i * m) % n;
+            if (alpha != i) {
+
+                // Get x,y cords for two points along circle.
+                // Starting point.
+                let x1 = c + r * Math.sin((i * 2 * Math.PI) / n);
+                let y1 = c - r * Math.cos((i * 2 * Math.PI) / n);
+
+                // Ending point.
+                let x2 = c + r * Math.sin((m * i * 2 * Math.PI) / n);
+                let y2 = c - r * Math.cos((m * i * 2 * Math.PI) / n);
+
+                // Create lines between these two points.
+                // let line = this.svgLine(x1, y1, x2, y2);
+                // console.log("line", x1, y1, x2, y2)
+                this.p5.stroke("black");
+                this.p5.strokeWeight(1);
+                this.p5.line(x1, y1, x2, y2);
+
+                // Set color and add to svg...
+                // let lineLength = Math.floor(
+                //     Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2).toFixed(2)
+                // );
+
+                // let colorNumber = this.remapInt(
+                //     lineLength,
+                //     0,
+                //     this.diameter,
+                //     96,
+                //     0
+                // );
+                // console.log(colors[colorNumber]);
+                // console.log(colorNumber)
+                // let max = 0xEEEEEE;
+                // let result = Math.round(max / colorNumber).toString(16);
+                // const linecolor = `hsl(${lineLength * 2}, 100%, 50%)`; // Color based on length
+
+                // line.setAttribute('stroke', colors[colorNumber]);
+                // line.setAttribute("stroke", linecolor);
+                // lines.push(line);
+            }
+        }
+        // return lines;
+    }
+
+
+}
+
+/*
+remapInt(val, low1, high1, low2, high2) {
+    let remapped = low2 + ((high2 - low2) * (val - low1)) / (high1 - low1);
+    return this.p5.Math.floor(remapped);
+}
+
+getModulus() {
+    return document.getElementById("modulus-number").value;
+}
+
+getMultiplier() {
+    return document.getElementById("multiplier-number").value;
+}
+
+updateModulus(val) {
+    document.getElementById("modulus-range").value = val;
+    document.getElementById("modulus-number").value = val;
+}
+
+updateMultiplier(val) {
+    document.getElementById("multiplier-range").value = val;
+    document.getElementById("multiplier-number").value = val;
+}
+
+buildSVG(w, h) {
+    let mySvg = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "svg"
+    );
+    mySvg.setAttribute("fill", "none");
+    mySvg.setAttribute("viewBox", "0 0 " + w + " " + h);
+    mySvg.setAttribute("id", "svg-total");
+    return mySvg;
+}
+
+svgLine(x1, y1, x2, y2) {
+    let myLine = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "line"
+    );
+    myLine.setAttribute("x1", parseInt(x1));
+    myLine.setAttribute("y1", parseInt(y1));
+    myLine.setAttribute("x2", parseInt(x2));
+    myLine.setAttribute("y2", parseInt(y2));
+    myLine.setAttribute("class", "svg-line");
+    return myLine;
+}
+
+svgCircle(center, radius) {
+    let myCircle = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "ellipse"
+    );
+    myCircle.setAttribute("cx", parseInt(center));
+    myCircle.setAttribute("cy", parseInt(center));
+    myCircle.setAttribute("rx", parseInt(radius));
+    myCircle.setAttribute("ry", parseInt(radius));
+    myCircle.setAttribute("stroke", "black");
+    myCircle.setAttribute("fill", "none");
+    myCircle.setAttribute("id", "svg-outline");
+    return myCircle;
+}
+
+svgText(x, y, text) {
+    let myText = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "text"
+    );
+    myText.setAttribute("x", parseInt(x));
+    myText.setAttribute("y", parseInt(y));
+    myText.setAttribute("stroke", "black");
+    myText.setAttribute("text-anchor", "middle");
+    myText.innerHTML = text;
+    console.log(myText);
+    return myText;
+}
+
+createColors() {
+    // Create 96 colors
+    let colors = [];
+
+    for (let r = 0; r <= 255; r += 8) {
+        let rgbR = "rgb(255," + r + ", 0)";
+        colors.push(rgbR);
+    }
+
+    for (let g = 0; g <= 255; g += 8) {
+        let rgbG = "rgb(0, 255, " + g + ")";
+        colors.push(rgbG);
+    }
+
+    for (let b = 0; b <= 255; b += 8) {
+        let rgbR = "rgb(" + b + ", 0, 255)";
+        colors.push(rgbR);
+    }
+    return colors;
+}
+
+draw() {
+    // Clear previous graph.
+    let svgGraph = document.getElementById("svg-container");
+    if (svgGraph.children.length >= 1) {
+        svgGraph.removeChild(svgGraph.children[0]);
+    }
+
+    // Get the current modulus and multiplier.
+    let modulus = this.getModulus();
+    let multiplier = this.getMultiplier();
+
+    // Create a new SVG element
+    let svg = this.buildSVG(this.svgWidth, this.svgHeight);
+
+    // Create outline and add to SVG.
+    let outline = this.svgCircle(this.center, this.radius);
+    svg.appendChild(outline);
+
+    // Add numbers to outside of outline
+    // for(let i = 0; i < modulus; i++) {
+    //   let x1 = this.center + (this.radius + 5) * Math.sin(i * 2 * Math.PI / modulus);
+    //   let y1 = this.center - (this.radius + 5) * Math.cos(i * 2 * Math.PI / modulus);
+    //   let number = this.svgText(x1, y1, i);
+    //   svg.appendChild(number);
+    // }
+
+    // Create and add lines to SVG.
+    let lines = this.createLines(
+        this.center,
+        this.radius,
+        multiplier,
+        modulus,
+        svg
+    );
+
+    for (let i = 0; i < lines.length; i++) {
+        svg.appendChild(lines[i]);
+    }
+
+    // Give SVG a class name.
+    svg.setAttribute("class", "vector-graph-svg");
+
+    // Add SVG to div.
+    document.getElementById("svg-container").appendChild(svg);
+}
+
+*/
+
+
+/*
+
+let myVortex = new Vortex();
+
+function updateModulus(value) {
+    myVortex.updateModulus(value);
+    myVortex.draw();
+}
+
+function updateMultiplier(value) {
+    myVortex.updateMultiplier(value);
+    myVortex.draw();
+}
+
+function downloadSVG() {
+    // myVortex.downloadSVG();
+
+    // Get the SVG container div.
+    let svgGraph = document.getElementById("svg-container");
+
+    // Get outline and change stroke to black.
+    let outline = document.getElementById("svg-outline");
+    outline.setAttribute("stroke", "black");
+
+    // Get each line and change stroke to blue.
+    let lines = document.getElementsByClassName("svg-line");
+    for (let i = 0; i < lines.length; i++) {
+        lines[i].setAttribute("stroke", "blue");
+    }
+
+    // Create a file to download from the SVG html,
+    // then download the file.
+    let svgFile = svgGraph.innerHTML;
+    let blob = new Blob([svgFile.toString()]);
+    let element = document.createElement("a");
+    element.download = "svg-coaster.svg";
+    element.href = window.URL.createObjectURL(blob);
+    element.click();
+    element.remove();
+}
+
+*/
