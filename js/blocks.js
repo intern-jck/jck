114974d112b754d183e5ab2ce456ac7967c7bf9f
Justin Kirk <justin.c.kirk@gmail.com>
2026-01-21 16:08:53 -0500

    need to work out blocks

--- js/blocks.js
+++ js/blocks.js
@@ -1,12 +1,15 @@
 const colors = ["red", "green", "blue", "magenta", "cyan"];
+
 let offset = 5;
 
 class Block {
-    constructor(x, y, w, h) {
+    constructor(x, y, w, h, c) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.c = c;
+
         this.speed = 0;
         this.gravity = 0.1;
         this.offsetX = 0;
@@ -14,10 +17,11 @@ class Block {
         this.dragging = false;
         this.over = false;
         this.active = false;
+        this.currentColor = this.c
     }
 
     show() {
-        fill(200)
+        fill(this.currentColor)
         stroke(0)
         strokeWeight(2)
         rect(this.x, this.y, this.w, this.h);
@@ -38,9 +42,11 @@ class Block {
         }
     }
 
-    checkCollision() {
-        if (this.atEdge) {
-            this.y = height;
+    checkCollision(block) {
+        if (this.y + this.h > block.y) {
+            this.currentColor = "purple"
+        } else {
+            this.currentColor = this.c
         }
     }
 
@@ -80,6 +86,7 @@ class Block {
 }
 
 let block = null;
+let block2 = null;
 
 function setup() {
     let c = document.getElementById("blocks");
@@ -90,8 +97,11 @@ function setup() {
     canvas.parent('blocks');
     frameRate(60);
 
-    block = new Block(50, 50, 50, 50)
+    block = new Block(50, 50, 50, 50, "blue")
     block.show();
+
+    block2 = new Block(150, 65, 50, 50, "green")
+    block2.show();
 }
 
 function draw() {
@@ -99,124 +109,21 @@ function draw() {
     block.show();
     block.update();
     block.hover();
+    block.checkCollision(block2);
+
+    block2.show();
+    block2.update();
+    block2.hover();
+    block2.checkCollision(block);
 }
 
 function mousePressed() {
     block.pressed();
+    block2.pressed();
 }
 
 function mouseReleased() {
     block.released();
+    block2.released();
 }
 
-
-
-/**
- Junkyard
-
-
-
-    // let xSpacing = width / 9;
-
-    // for (let i = 0; i < 9; i += 1) {
-    //     let mass = random(0.5, 3);
-    //     let xPosition = xSpacing * i + xSpacing / 2;
-    //     blocks[i] = new Block(mass, xPosition, 0, color(i, 100, 100));
-    // }
-
-    // for (let block of blocks) {
-
-    //     // Gravitational force is proportional to the mass
-    //     // let gravity = createVector(0, 0.1 * block.mass);
-    //     // let normalForce = createVector(0, 0)
-    //     // // Apply gravitational force
-    //     // if (block.atEdge()) {
-    //     //     block.applyForce(normalForce);
-    //     // } else {
-    //     //     block.applyForce(gravity);
-    //     // }
-
-    //     // Update and display
-    //     block.update();
-    //     block.display();
-    //     block.checkEdges();
-    // }
-
-
-    // shape1.show();
-    // shape2.show();
-    // shape1.hover();
-    // shape2.hover();
-    // shape1.update();
-    // shape2.update();
-
- 
-let shape1 = new Rectangle(25, 25, 25, 25);
-let shape2 = new Rectangle(50, 50, 50, 50);
-
-let width = 0;
-let height = 0;
-let blocks = [];
-class Block {
-    constructor(m, x, y, c) {
-        this.mass = 1;
-        // this.position = createVector(x, y);
-        // this.velocity = createVector(0, 0);
-        // this.acceleration = createVector(0, 0);
-        this.color = c;
-        this.x = x;
-        this.y = y;
-        this.w = 25;
-        this.h = 25;
-        // this.f = createVector(0, 0);
-    }
-
-    // Apply force according to Newton's 2nd law: F = M * A
-    // or A = F / M
-    // applyForce(force) {
-    //     this.f = p5.Vector.div(force, this.mass);
-    //     this.acceleration.add(this.f);
-    // }
-
-    update() {
-        // Change the velocity by the acceleration
-        // this.velocity.add(this.acceleration);
-
-        // // Change the position by the velocity
-        // this.position.add(this.velocity);
-
-        // // Clear the acceleration each frame
-        // this.acceleration.mult(0);
-        this.y += 1;
-    }
-
-    display() {
-        stroke(0);
-        strokeWeight(2);
-        fill(this.color);
-        ellipse(this.x, this.y, this.w, this.h);
-    }
-
-    // Make the balls bounce at the bottom
-    // checkEdges() {
-    //     // if (this.position.y > height - this.h) {
-    //     //     // A little dampening when hitting the bottom
-    //     //     // this.velocity.y *= -0.1;
-    //     //     this.position.y = height - this.h;
-    //     //     // this.
-    //     // }
-    //     if (this.atEdge) {
-    //         console.log("edge!")
-    //         this.y = height;
-    //     }
-    // }
-
-    // atEdge() {
-    //     if (this.position.y > height - this.h) {
-    //         return true
-    //     }
-
-    //     return false
-    // }
-}
- */
